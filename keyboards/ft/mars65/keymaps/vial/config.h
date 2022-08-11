a01f79f7609a6f65c2ea5fe44dfabab6c7fda647
Alberto <albertopastrolin@gmail.com>
2022-08-11 15:09:44 +0200

    fix: typo fix

--- keyboards/ft/mars65/keymaps/vial/config.h
+++ keyboards/ft/mars65/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0xE2, 0xA3, 0x39, 0x89, 0xAC, 0x3E, 0x2D, 0x55}
 
-#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
 #define VIAL_UNLOCK_COMBO_COLS {0, 14}
