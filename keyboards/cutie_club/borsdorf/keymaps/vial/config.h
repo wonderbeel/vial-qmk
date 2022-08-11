4bfa6c7e5ebc778c2940df083a664c7b01fb4876
Alberto <albertopastrolin@gmail.com>
2022-08-11 15:03:39 +0200

    feat: ported to vial

---
+++ keyboards/cutie_club/borsdorf/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x90, 0x47, 0xEB, 0xAC, 0x03, 0xB0, 0xA4, 0x84}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
