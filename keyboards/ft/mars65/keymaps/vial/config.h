9c0194bfa3af73dd4d696b00c916c748413fa2ee
Alberto <albertopastrolin@gmail.com>
2022-08-11 15:04:23 +0200

    feat: ported to vial

---
+++ keyboards/ft/mars65/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE2, 0xA3, 0x39, 0x89, 0xAC, 0x3E, 0x2D, 0x55}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 14}
